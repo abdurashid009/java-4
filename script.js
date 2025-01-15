eb60cd8065edf034493b40888524dee9d2cee0cf
Abdurashid <abdurashidnormetov2009.26@gmail.com>
2025-01-15 15:31:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let first = prompt('String typegi malumot kirgazing')
+let two = prompt('Number typegi malumot kirgazing')
+let three = prompt('Boolean typegi malumot kirgazing')
+let free = confirm("Hamma malumotni to'g'ri kiritganingizga ishonchingiz komilmi?")
+
+alert(`
+Siz kiritgan ${first} sozi string typega tegishli
+Siz kiritgan ${two} number typega tegishli
+Siz kiritgan ${three} boolean typega tegishli
+`)
